f30b50970e8c9ff6182f5fa362446f02cef594ac
James Halliday <mail@substack.net>
2014-01-15 22:38:13 -0800

    opt for a value-based instead of attribute-name based signature

--- index.js
+++ index.js
@@ -18,27 +18,33 @@ Attractor.prototype.add = function (key, cb) {
     var ex = expand(key);
     
     for (var i = 0; i < ex.length; i++) {
-        var sel = '*[' + ex[i] + ']';
-        var s = this._selectors[sel];
-        if (!s) s = this._selectors[sel] = [];
-        s.push({ index: i, attr: ex[i], cb: cb });
+        var s = this._selectors[ex[i]];
+        if (!s) s = this._selectors[ex[i]] = { ex: ex, cb: [] };
+        s.cb.push(cb);
     }
 };
 
 Attractor.prototype.scan = function (root) {
     var keys = objectKeys(this._selectors);
     for (var i = 0; i < keys.length; i++) {
-        var sel = keys[i];
-        var attr = this._selectors[sel];
+        var key = keys[i];
+        var sel = '*[' + key + ']';
+        var rec = this._selectors[key];
+        
         var elems = root.querySelectorAll(sel);
-        if (root.getAttribute && root.getAttribute(attr[0].attr)) {
+        if (root.getAttribute && root.getAttribute(key)) {
             elems = [].slice.call(elems);
             elems.unshift(root);
         }
         
         for (var j = 0; j < elems.length; j++) {
-            for (var k = 0; k < attr.length; k++) {
-                attr[k].cb.call(this, elems[j], attr[k].attr, attr[k].index);
+            var args = [ elems[j] ];
+            for (var k = 0; k < rec.ex.length; k++) {
+                args.push(elems[j].getAttribute(rec.ex[k]));
+            }
+            
+            for (var k = 0; k < rec.cb.length; k++) {
+                rec.cb[k].apply(this, args);
             }
         }
     }
