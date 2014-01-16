ffd1922fab4ce3ce90f3b48acb46f35f0a5ef602
James Halliday <mail@substack.net>
2014-01-15 23:11:00 -0800

    rearrange the live example code

--- example/live/browser.js
+++ example/live/browser.js
@@ -1,8 +1,35 @@
-var render = require('./render.js');
-var db = require('multilevel-feed')();
-var observe = require('observ');
-var active = observe();
+var attractor = require('../../');
+var range = require('attr-range');
+var submit = require('attr-submit');
+var chooser = require('attr-chooser');
 
+var render = require('./render.js');
+var observe = require('observ');
+
+var db = require('multilevel-feed')();
+var sock = require('shoe')('/sock');
+sock.pipe(db.createRpcStream()).pipe(sock);
+
+var attr = attractor();
+
+attr.add('data-start', range(function (rel) {
+    var r = db.livefeed(rel.range).pipe(render());
+    r.on('element', function (elem) { attr.scan(elem) });
+    r.sortTo(rel.element, '.score');
+}));
+
+attr.add('x-submit', submit(function (form, fields) {
+    db.put('item!' + fields.title, { score: 0, title: fields.title });
+    form.reset();
+}));
+
+attr.add('x-chooser', chooser('active', function (elem) {
+    active.set(elem.querySelector('.title').textContent);
+}));
+
+attr.scan(document);
+
+var active = observe();
 active(function (txt) {
     document.querySelector('#active').textContent = txt;
     document.querySelector('#vote').classList.remove('hide');
@@ -15,29 +42,3 @@ document.querySelector('#vote').addEventListener('click', function (ev) {
         db.put(key, value);
     });
 });
-
-var live = require('attr-range')(function (rel) {
-    var r = db.livefeed(rel.range).pipe(render());
-    r.on('element', function (elem) { attr.scan(elem) });
-    r.sortTo(rel.element, '.score');
-});
-
-var chooser = require('attr-chooser')('active', function (elem) {
-    active.set(elem.querySelector('.title').textContent);
-});
-
-var submit = require('attr-submit')(function (form, fields) {
-    db.put('item!' + fields.title, { score: 0, title: fields.title });
-    form.reset();
-});
-
-var attractor = require('../../');
-var attr = attractor({
-    'data-start': live,
-    'x-submit': submit,
-    'x-chooser': chooser
-});
-attr.scan(document);
-
-var sock = require('shoe')('/sock');
-sock.pipe(db.createRpcStream()).pipe(sock);
