53467f305d5eff2c72d5323a442c4490d4f57035
James Halliday <mail@substack.net>
2014-01-18 15:00:08 -0800

    ranges added into the new example

--- example/live/browser.js
+++ example/live/browser.js
@@ -1,5 +1,6 @@
 var attractor = require('../../');
 var observe = require('observable');
+var render = require('./render.js');
 
 window.db = require('multilevel-feed')();
 var sock = require('shoe')('/sock');
@@ -8,12 +9,20 @@ sock.pipe(db.createRpcStream()).pipe(sock);
 function SortedList () {
     this.active = observe('');
     this.active(function (txt) {
+console.log('txt=', txt); 
         if (!txt) return;
         document.querySelector('h2').classList.remove('hide');
     });
 }
 
+SortedList.prototype.range = function (xr) {
+    var r = db.livefeed(xr.range).pipe(render());
+    r.on('element', function (elem) { attr.scan(elem) });
+    r.sortTo(xr.element, '.score');
+};
+
 SortedList.prototype.select = function (elem) {
+console.log('SELECT');
     this.active(elem.querySelector('.title').textContent);
 };
 
@@ -22,6 +31,8 @@ SortedList.prototype.addItem = function () {
 };
 
 var attr = attractor({
-    'x-chooser': [ require('attr-chooser'), 'active' ]
+    'x-chooser': [ require('attr-chooser'), 'active' ],
+    'x-bind': require('attr-bind'),
+    'x-range': [ require('attr-range'), 'data-start', 'data-end' ]
 }, new SortedList);
 attr.scan(document);
