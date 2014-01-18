e035d8814562fbd10fd62aacc4d1f94a46ad9dbf
James Halliday <mail@substack.net>
2014-01-17 19:45:12 -0800

    fix the example, code was fine

--- example/live/browser.js
+++ example/live/browser.js
@@ -4,11 +4,6 @@ var observe = require('observable');
 window.db = require('multilevel-feed')();
 var sock = require('shoe')('/sock');
 sock.pipe(db.createRpcStream()).pipe(sock);
-
-var attr = attractor({
-    'x-chooser': [ require('attr-chooser'), 'active' ]
-}, new SortedList);
-attr.scan(document);
  
 function SortedList () {
     this.active = observe('');
@@ -19,10 +14,14 @@ function SortedList () {
 }
 
 SortedList.prototype.select = function (elem) {
-console.log('SELECT!!!', elem);
     this.active(elem.querySelector('.title').textContent);
 };
 
 SortedList.prototype.addItem = function () {
     console.log('TODO');
 };
+
+var attr = attractor({
+    'x-chooser': [ require('attr-chooser'), 'active' ]
+}, new SortedList);
+attr.scan(document);
