d3f3ff8b4361a8cc7e40088132eb2099ef31e9ec
James Halliday <mail@substack.net>
2014-01-17 18:20:48 -0800

    mindfuck finally works

--- example/live/browser.js
+++ example/live/browser.js
@@ -1,50 +1,28 @@
 var attractor = require('../../');
-var range = require('attr-range');
-var submit = require('attr-submit');
-var chooser = require('attr-chooser');
-
-var render = require('./render.js');
 var observe = require('observable');
 
-var db = require('multilevel-feed')();
+window.db = require('multilevel-feed')();
 var sock = require('shoe')('/sock');
 sock.pipe(db.createRpcStream()).pipe(sock);
 
-var attr = attractor();
-
-attr.add('data-start', range(function (rel) {
-    var r = db.livefeed(rel.range).pipe(render());
-    r.on('element', function (elem) { attr.scan(elem) });
-    r.sortTo(rel.element, '.score');
-}));
-
-attr.add('x-submit', submit(function (form, fields) {
-    db.put('item!' + fields.title, { score: 0, title: fields.title });
-    form.reset();
-}));
-
-attr.add('x-chooser', chooser('active', function (elem) {
-    active(elem.querySelector('.title').textContent);
-}));
-
-var bind = require('attr-bind')();
-attr.add('x-bind', bind);
-
-var active = observe();
-bind(active, 'active');
-
-active(function (txt) {
-    if (!txt) return;
-    document.querySelector('h2').classList.remove('hide');
-});
-
-document.querySelector('#vote').addEventListener('click', function (ev) {
-    var key = 'item!' + active();
-    db.get(key, function (err, value) {
-        value.score += 5;
-        db.put(key, value);
-    });
-});
-
+var attr = attractor({
+    'x-chooser': [ require('attr-chooser'), 'active' ]
+}, new SortedList);
 attr.scan(document);
-window.db = db;
+ 
+function SortedList () {
+    this.active = observe('');
+    this.active(function (txt) {
+        if (!txt) return;
+        document.querySelector('h2').classList.remove('hide');
+    });
+}
+
+SortedList.prototype.select = function (elem) {
+console.log('SELECT!!!', elem);
+    this.active(elem.querySelector('.title').textContent);
+};
+
+SortedList.prototype.addItem = function () {
+    console.log('TODO');
+};
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var expand = require('brace-expansion');
-
+var isArray = require('isarray');
 module.exports = Attractor;
 
-function Attractor (attrs) {
-    if (!(this instanceof Attractor)) return new Attractor(attrs);
+function Attractor (attrs, scope) {
+    if (!(this instanceof Attractor)) return new Attractor(attrs, scope);
     
     this._selectors = {};
+    this.scope = scope || {};
     
     var keys = objectKeys(attrs || {});
     for (var i = 0; i < keys.length; i++) {
@@ -14,17 +15,41 @@ function Attractor (attrs) {
     }
 }
 
+Attractor.prototype.lookup = function (str) {
+    var keys = str.split('.');
+    var node = this.scope, context = null;
+    for (var i = 0; i < keys.length; i++) {
+        context = node;
+        node = node[keys[i]];
+        if (!node) return null;
+    }
+    return { context: context, value: node };
+};
+
 Attractor.prototype.add = function (key, cb) {
     var ex = expand(key);
-    
-    for (var i = 0; i < ex.length; i++) {
-        var s = this._selectors[ex[i]];
-        if (!s) s = this._selectors[ex[i]] = { ex: ex, cb: [] };
-        s.cb.push(cb);
+    if (isArray(cb)) {
+        cb = (function (args) {
+            if (typeof args[0] !== 'function') {
+                throw new Error(
+                    'first element in .add(array) must be a function'
+                );
+            }
+            return function () {
+                var argv = args.slice(1).concat([].slice.call(arguments));
+                return args[0].apply(this, argv);
+            };
+        })(cb);
     }
+    
+    var s = this._selectors[ex[0]];
+    if (!s) s = this._selectors[ex[0]] = [];
+    
+    s.push({ extra: ex.slice(1), cb: cb });
 };
 
 Attractor.prototype.scan = function (root) {
+    var self = this;
     var keys = objectKeys(this._selectors);
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i];
@@ -36,15 +61,33 @@ Attractor.prototype.scan = function (root) {
             elems = [].slice.call(elems);
             elems.unshift(root);
         }
+        if (elems.length === 0) continue;
         
-        for (var j = 0; j < elems.length; j++) {
-            var args = [ elems[j] ];
-            for (var k = 0; k < rec.ex.length; k++) {
-                args.push(elems[j].getAttribute(rec.ex[k]));
-            }
-            
-            for (var k = 0; k < rec.cb.length; k++) {
-                rec.cb[k].apply(this, args);
+        for (var n = 0; n < rec.length; n++) {
+            var r = rec[n];
+            for (var j = 0; j < elems.length; j++) {
+                var values = [ elems[j].getAttribute(key) ];
+                for (var k = 0; k < r.extra.length; k++) {
+                    var v = elems[j].getAttribute(r.extra[k]);
+                    if (v === null || v === undefined) break;
+                }
+                if (k !== r.extra.length) continue;
+                
+                if (typeof r.cb !== 'function') continue;
+                var f = r.cb((function (elem) {
+                    return function () {
+                        for (var k = 0; k < values.length; k++) {
+                            var p = self.lookup(values[k]);
+                            if (!p) continue;
+                            if (p && typeof p.value === 'function') {
+                                p.value.call(p.context, elem);
+                            }
+                        }
+                    };
+                })(elems[j]));
+                if (typeof f === 'function') {
+                    f.apply(this, [ elems[j] ].concat(values));
+                }
             }
         }
     }
