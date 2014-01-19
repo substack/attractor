69af087a744713128a6ae9fce49eca31552e2e8b
James Halliday <mail@substack.net>
2014-01-19 11:41:12 -0800

    addItem() works for new entries

--- example/live/browser.js
+++ example/live/browser.js
@@ -9,7 +9,6 @@ sock.pipe(db.createRpcStream()).pipe(sock);
 function SortedList () {
     this.active = observe('');
     this.active(function (txt) {
-console.log('TXT=', txt); 
         if (!txt) return;
         document.querySelector('h2').classList.remove('hide');
     });
@@ -22,17 +21,18 @@ SortedList.prototype.range = function (xr) {
 };
 
 SortedList.prototype.select = function (elem) {
-console.log('SELECT');
     this.active(elem.querySelector('.title').textContent);
 };
 
-SortedList.prototype.addItem = function () {
-    console.log('TODO');
+SortedList.prototype.addItem = function (form, fields) {
+    db.put('item!' + fields.title, { score: 0, title: fields.title });
+    form.reset();
 };
 
 var attr = attractor({
     'x-chooser': [ require('attr-chooser'), 'active' ],
     'x-bind': require('attr-bind'),
-    'x-range': [ require('attr-range'), 'data-start', 'data-end' ]
+    'x-range': [ require('attr-range'), 'data-start', 'data-end' ],
+    'x-submit': require('attr-submit')
 }, new SortedList);
 attr.scan(document);
