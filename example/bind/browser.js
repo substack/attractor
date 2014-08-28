5ef85558167fa2e873c48fae9db1de0351a2443f
James Halliday <mail@substack.net>
2014-08-27 17:40:07 -0700

    use scoped observables in the element updates

--- example/bind/browser.js
+++ example/bind/browser.js
@@ -1,4 +1,8 @@
 var attractor = require('../../');
-var scope = { yourName: function (txt) { console.log(txt) } };
+var scope = {
+    yourName: function (txt) {
+        console.log('yourname=', txt)
+    }
+};
 var attr = attractor({ 'binder': require('attr-bind') }, scope);
 attr.scan(document);
